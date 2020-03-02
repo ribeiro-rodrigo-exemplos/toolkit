562b7f2a5f59110ac9af26595fc15c88dda4b28e
rodrigo.santos <ribeiro.santos@g.globo>
2020-03-02 13:17:08 -0300

    implementacao inicial da tela de listagem de buckets

--- src/views/Routes.tsx
+++ src/views/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 // Import Pages 
 import Dashboard from './pages/Dashboard/'
@@ -10,7 +10,7 @@ import MFA from './pages/MFA/'
 export const appRoutes: any = {
     "/": { name: "Home", exact: true, component: Dashboard },
     "/dashboard": { name: "Dashboard", component: Dashboard },
-    "/find": { name: "Pesquisa", component: Dashboard },
+    "/find": { name: "Pesquisa", component: Dashboard, hasSubRoutes: true },
     "/find/buckets": { name: "Buckets", component: FindBuckets },
     "/find/machines": { name: "Maquinas", component: FindMachines },
     "/mfa": { name: "MFA", component: MFA }
@@ -26,7 +26,9 @@ export default () => (
                 if (routeObject.exact)
                     return <Route key={path} exact path={path} component={routeObject.component} />
                 else
-                    return <Route key={path} path={path} component={routeObject.component} />
+                    if(!routeObject.hasSubRoutes)
+                        return <Route key={path} path={path} component={routeObject.component} />
+                return null 
             })
         }
     </Switch>
--- src/views/pages/find/Buckets/Buckets.tsx
+++ src/views/pages/find/Buckets/Buckets.tsx
@@ -1,5 +1,96 @@
 import React from 'react'
 
+import { 
+    Row, 
+    Col, 
+    Card, 
+    CardHeader, 
+    CardBody, 
+    Table, 
+    FormGroup, 
+    Label, 
+    Input, 
+    Button 
+} from 'reactstrap'
+
 export default () => (
-    <h1>Buckets</h1>
+    <div className="animated fadeIn">
+        <Row>
+            <Col xs="6" lg="12">
+                <Card>
+                    <CardHeader>
+                        <i className="fa fa-search"></i> Filtros
+                    </CardHeader>
+                    <CardBody>
+                        <Row>
+                            <Col lg="3">
+                                <FormGroup>
+                                    <Label htmlFor="nome">Nome</Label>
+                                    <Input type="text" id="name" placeholder="Digite o nome da bucket"/> 
+                                </FormGroup>
+                            </Col>
+                            <Col lg="3">
+                                <FormGroup>
+                                    <Label htmlFor="provider">Cloud Provider</Label>
+                                    <Input type="select" id="provider">
+                                        <option value="all">Todos</option>
+                                        <option value="aws">Amazon Webservices</option>
+                                        <option value="gcp">Google Cloud</option>
+                                        <option value="azure">Azure</option>
+                                    </Input>
+                                </FormGroup>
+                            </Col>
+                            <Col lg="3">
+                                <FormGroup>
+                                    <Label htmlFor="data-criacao">Data de Criação</Label>
+                                    <Input type="date" id="data-criacao"/> 
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs="6" lg="12">
+                                <Button color="secondary" className="float-right">Limpar</Button>
+                                <Button color="primary" className="float-right horizontal-margin-right-5" >Buscar</Button>
+                            </Col>
+                        </Row>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+        <Row>
+            <Col xs="6" lg="12">
+                <Card>
+                    <CardHeader>
+                        <i className="fa fa-align-justify"></i> Buckets Filtrados
+                    </CardHeader>
+                    <CardBody>
+                        <Table responsive bordered striped hover size="sm">
+                            <thead>
+                                <tr>
+                                    <th>Nome</th>
+                                    <th>Dono</th>
+                                    <th>Data de Criação</th>
+                                    <th>Cloud Provider</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Bucket 1</td>
+                                    <td>masteraccount</td>
+                                    <td>23/02/2019</td>
+                                    <td>Amazon Webservices</td>
+                                </tr>
+                                <tr>
+                                    <td>Bucket 2</td>
+                                    <td>masteraccount</td>
+                                    <td>19/02/2019</td>
+                                    <td>Amazon Webservices</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+    </div>
 )
