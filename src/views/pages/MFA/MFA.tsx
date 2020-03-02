c3effc43a75af2e2f4f726d30301c24a8ea92312
rodrigo.santos <ribeiro.santos@g.globo>
2020-03-02 16:34:45 -0300

    implementacao inicial da tela de mfa/contas

--- src/views/pages/MFA/MFA.tsx
+++ src/views/pages/MFA/MFA.tsx
@@ -1,5 +1,41 @@
 import React from 'react'
 
+import {
+    Col, 
+    Row, 
+    Table, 
+    Card, 
+    CardHeader, 
+    CardBody
+} from 'reactstrap'
+
 export default () => (
-    <h1>MFA</h1>
+    <div className="animated fadeIn">
+        <Row>
+            <Col xs="6" lg="12">
+                <Card>
+                    <CardHeader>
+                        <i className="fa fa-align-justify"></i>Lista de MFAs
+                    </CardHeader>
+                    <CardBody>
+                        <Table hover bordered striped size="md">
+                            <thead>
+                                <tr>
+                                    <th>Conta</th>
+                                    <th>ID</th>
+                                    <th>Email</th>
+                                    <th>MFA</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+    </div>
 )
