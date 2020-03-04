5dca2a3261fdc3f4e5dfbccf4d24205382fa7fb9
rodrigo.santos <ribeiro.santos@g.globo>
2020-03-04 11:06:57 -0300

    viewmodel

--- src/viewModels/accounts/AccountsViewModel.ts
+++ src/viewModels/accounts/AccountsViewModel.ts
@@ -1,4 +1,14 @@
+import account from '../../models/entities/account'
+import { observable } from 'mobx'
+
 
 class AccountsViewModel{
+
+    @observable
+    private _accounts: account[]
+
+    get accounts(){
+        return this._accounts
+    }
     
 }
