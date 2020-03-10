741de4085bd602c3b02d8f5ca25ca350cee1d771
rodrigo.santos <ribeiro.santos@g.globo>
2020-03-10 13:51:14 -0300

    aumentando o tamanho do spinner e subindo ele mais um pouco

--- src/app/views/components/Spinner/Spinner.tsx
+++ src/app/views/components/Spinner/Spinner.tsx
@@ -10,9 +10,9 @@ export default (): any => {
                 style={{
                     position: "absolute", 
                     left: "50%", 
-                    top: "50%"
+                    top: "40%"
                 }}>
-                <svg width="100" height="100" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg" fill="#20a8d8" aria-label="audio-loading">
+                <svg width="150" height="150" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg" fill="#20a8d8" aria-label="audio-loading">
                     <circle cx="15" cy="15" r="11.9215">
                         <animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15" calcMode="linear" repeatCount="indefinite"></animate>
                         <animate attributeName="fillOpacity" from="1" to="1" begin="0s" dur="0.8s" values="1;.5;1" calcMode="linear" repeatCount="indefinite"></animate>
